c5d0dd49d8183fd638b21a684dc11968681d191c
Patrick Attimont <patrickattimont@gmail.com>
2026-08-27 10:38:57 +0200

    Fixed Triangle class

--- NexusBVH/include/NXB/Triangle.h
+++ NexusBVH/include/NXB/Triangle.h
@@ -14,12 +14,12 @@ namespace NXB
 		__host__ __device__ Triangle(float3 pos0, float3 pos1, float3 pos2)
 			: v0(pos0), v1(pos1), v2(pos2) { }
 
-		__host__ __device__ float3 Centroid()
+		__host__ __device__ float3 Centroid() const
 		{
-			return (v0 + v1 + v2) * 0.5;
+			return (v0 + v1 + v2) / 3.0f;
 		}
 
-		__host__ __device__ AABB Bounds()
+		__host__ __device__ AABB Bounds() const
 		{
 			return AABB(v0, v1, v2);
 		}
